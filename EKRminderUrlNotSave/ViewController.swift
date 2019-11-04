98cd9f32494d72231db7a4e12a63de60d6e5dd2c
Kyosuke Takayama <loiseau@gmail.com>
2019-11-04 11:43:14 +0900

    Initial Commit

---
+++ EKRminderUrlNotSave/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EKRminderUrlNotSave
+//
+//  Created by Kyosuke Takayama on 2019/11/04.
+//  Copyright © 2019 Kyosuke Takayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
